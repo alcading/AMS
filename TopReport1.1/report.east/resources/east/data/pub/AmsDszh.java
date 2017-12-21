a1b0e72e32b3f48ac7b6cb6a1f9460ce86e089a3
tianqinfeng <tianqinfeng@cibfintech.com>
2017-12-21 16:08:02 +0800

    1.数据库新建对私账户表AMS_DSZH;2.应用新建hibernate映射关系及Javabean：AmsDszh.hbm.xml,AmsDszh.java,AmsDszhId.java,BaseAmsDszh.java,BaseAmsDszhId.java

---
+++ TopReport1.1/report.east/resources/east/data/pub/AmsDszh.java
@@ -0,0 +1,30 @@
+package resources.east.data.pub;
+
+
+
+import resources.east.data.base.BaseAmsDszh;
+
+/**
+ * EastGrhqckfhzmxjlTmp entity. @author MyEclipse Persistence Tools
+ */
+public class AmsDszh extends BaseAmsDszh implements java.io.Serializable {
+
+	// Constructors
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2007963389690146692L;
+
+	/** default constructor */
+	public AmsDszh() {
+	}
+
+	/** minimal constructor */
+	public AmsDszh(AmsDszhId id) {
+		super(id);
+	}
+
+
+
+}
